91dfebeb24ae79fa0bb9b7e43f2a513ff2307b12
Jason <Jason@Jasons-Macbook.local>
2018-04-16 20:41:52 +0700

    1st comment

---
+++ Session1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Session1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+         
+        }
+    }
+}
