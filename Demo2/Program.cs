a9c8b1d01ae9ab63017dddf55f6b66751509ee67
Jason <Jason@Jasons-Macbook.local>
2018-04-16 22:31:02 +0700

    demo 2

---
+++ Demo2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello demo 2 project");
+        }
+    }
+}
